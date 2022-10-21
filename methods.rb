d64bcbe789d95f00f38a7c4753344aec41d3bc42
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-10-21 08:11:03 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end 
+
+def greet(name)
+    puts "Hello, #{name}!"
+end 
+
+def greet_with_default(name ="programmer")
+    puts "Hello, #{name}!"
+end 
+
+
+def add (num1,num2)
+
+return num1 +num2
+
+
+end 
+sum = add(34,56) +add(67,54)
+
+
+def halve(num)
+if num.class != Integer
+    return nil
+    end
+    num/2
+end 
+ans=(839462667372/2)
+
+
